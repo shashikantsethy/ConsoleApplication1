ee8bbccc71f159ce0ee8e68f7061a6b30ee71157
Shashikant Sethy <shashikantsethy@gmail.com>
2015-09-12 16:50:45 +0530

    change2

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,13 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter Your name");
+            Console.ReadLine();
+
+
+
+
+
         }
     }
 }
